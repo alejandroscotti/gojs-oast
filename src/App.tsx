8353b5e6deadb0c35f7a7ea965523951f6da4d10
Alejandro Sebastian Scotti <57762924+alejandroscotti@users.noreply.github.com>
2024-01-28 21:30:31 -0500

    Install Redux and tweak Drawer logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-/*
- *  Copyright (C) 1998-2022 by Northwoods Software Corporation. All Rights Reserved.
- */
-
-import { useImmer } from "use-immer";
 import { useRef } from "react";
 import { ReactDiagram, ReactPalette } from "gojs-react";
+import { RootState, useAppDispatch, useAppSelector } from "./store/store";
+import {
+  onModelChange,
+  clearSelection,
+  changedSelection,
+} from "./store/slices";
 import * as go from "gojs";
 import {
   Checkbox,
@@ -14,174 +15,36 @@ import {
   FormLabel,
 } from "@mui/material";
 import GoJSWrapper from "./components/GoJSWrapper";
-import { diagramDataInit } from "./components/dataSource/diagramData";
-import { paletteDataInit } from "./components/dataSource/paletteData";
 import goJsCategory from "./components/dataSource/goJsCategoryConfiguration";
 import "./App.css";
 
-export default function App() {
+const App = () => {
+  const dispatch = useAppDispatch();
+
+  const { linkDataArray, nodeDataArray, paletteData, skipsDiagramUpdate } =
+    useAppSelector((state: RootState) => state.goJs);
+
   // Defined React GoJs Refs
   const diagramRef = useRef<ReactDiagram>(null);
   const paletteRef = useRef<ReactPalette>(null);
-
-  const [diagramData, updateDiagramData] = useImmer<any>({
-    linkDataArray: diagramDataInit.linkDataArray,
-    nodeDataArray: diagramDataInit.nodeDataArray,
-    paletteData: paletteDataInit,
-    selectedData: null,
-    skipsDiagramUpdate: false,
-  });
-
-  // maps for faster state modification
-  const mapNodeKeyIdx = new Map();
-  const mapLinkKeyIdx = new Map();
-
-  const refreshNodeIndex = (nodeArr: any) => {
-    mapNodeKeyIdx.clear();
-    nodeArr.forEach((n: any, idx: any) => {
-      mapNodeKeyIdx.set(n.key, idx);
-    });
-  };
-
-  const refreshLinkIndex = (linkArr: any) => {
-    mapLinkKeyIdx.clear();
-    linkArr.forEach((l: any, idx: any) => {
-      mapLinkKeyIdx.set(l.key, idx);
-    });
-  };
-
-  refreshNodeIndex(diagramData.nodeDataArray);
-  refreshLinkIndex(diagramData.linkDataArray);
 
   const handleDiagramEvent = (e: any) => {
     const name = e.name;
     switch (name) {
       case "ChangedSelection": {
         const sel = e.subject.first();
-        updateDiagramData((draft: any) => {
-          if (sel) {
-            if (sel instanceof go.Node) {
-              const idx = mapNodeKeyIdx.get(sel.key);
-              if (idx !== undefined && idx >= 0) {
-                const nd = draft.nodeDataArray[idx];
-                draft.selectedData = nd;
-              }
-            } else if (sel instanceof go.Link) {
-              const idx = mapLinkKeyIdx.get(sel.key);
-              if (idx !== undefined && idx >= 0) {
-                const ld = draft.linkDataArray[idx];
-                draft.selectedData = ld;
-              }
-            }
-          } else {
-            draft.selectedData = null;
-          }
-        });
+
+        if (sel) {
+          dispatch(changedSelection(sel));
+        } else {
+          dispatch(clearSelection());
+        }
+
         break;
       }
       default:
         break;
     }
-  };
-
-  const handleModelChange = (obj: any) => {
-    const insertedNodeKeys = obj.insertedNodeKeys;
-    const modifiedNodeData = obj.modifiedNodeData;
-    const removedNodeKeys = obj.removedNodeKeys;
-    const insertedLinkKeys = obj.insertedLinkKeys;
-    const modifiedLinkData = obj.modifiedLinkData;
-    const removedLinkKeys = obj.removedLinkKeys;
-
-    // maintain maps of modified data so insertions don't need slow lookups
-    const modifiedNodeMap = new Map();
-    const modifiedLinkMap = new Map();
-
-    updateDiagramData((draft: any) => {
-      let nodeDataArray = draft.nodeDataArray;
-
-      if (modifiedNodeData) {
-        modifiedNodeData.forEach((node: any) => {
-          modifiedNodeMap.set(node.key, node);
-          const idx = mapNodeKeyIdx.get(node.key);
-
-          if (idx !== undefined && idx >= 0) {
-            nodeDataArray[idx] = node;
-            if (draft.selectedData && draft.selectedData.key === node.key) {
-              draft.selectedData = node;
-            }
-          }
-        });
-      }
-
-      if (insertedNodeKeys) {
-        insertedNodeKeys.forEach((key: any) => {
-          const node = modifiedNodeMap.get(key);
-          const idx = mapNodeKeyIdx.get(key);
-
-          if (node && idx === undefined) {
-            // nodes won't be added if they already exist
-            mapNodeKeyIdx.set(node.key, nodeDataArray.length);
-            nodeDataArray.push(node);
-          }
-        });
-      }
-
-      if (removedNodeKeys) {
-        nodeDataArray = nodeDataArray.filter((nd: any) => {
-          if (removedNodeKeys.includes(nd.key)) {
-            return false;
-          }
-          return true;
-        });
-
-        draft.nodeDataArray = nodeDataArray;
-        refreshNodeIndex(nodeDataArray);
-      }
-
-      let linkDataArray = draft.linkDataArray;
-
-      if (modifiedLinkData) {
-        modifiedLinkData.forEach((ld: any) => {
-          modifiedLinkMap.set(ld.key, ld);
-          const idx = mapLinkKeyIdx.get(ld.key);
-
-          if (idx !== undefined && idx >= 0) {
-            linkDataArray[idx] = ld;
-
-            if (draft.selectedData && draft.selectedData.key === ld.key) {
-              draft.selectedData = ld;
-            }
-          }
-        });
-      }
-
-      if (insertedLinkKeys) {
-        insertedLinkKeys.forEach((key: any) => {
-          const linkData = modifiedLinkMap.get(key);
-          const idx = mapLinkKeyIdx.get(key);
-
-          if (linkData && idx === undefined) {
-            // links won't be added if they already exist
-            mapLinkKeyIdx.set(linkData.key, linkDataArray.length);
-            linkDataArray.push(linkData);
-          }
-        });
-      }
-
-      if (removedLinkKeys) {
-        linkDataArray = linkDataArray.filter((ld: any) => {
-          if (removedLinkKeys.includes(ld.key)) {
-            return false;
-          }
-          return true;
-        });
-
-        draft.linkDataArray = linkDataArray;
-        refreshLinkIndex(linkDataArray);
-      }
-
-      draft.skipsDiagramUpdate = true; // the GoJS model already knows about these updates
-    });
   };
 
   const handleLinkChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -216,47 +79,60 @@ export default function App() {
     diagram.commitTransaction();
   };
 
-  const getImportNodeLinks = (importNode: go.Part) => {
-    const docletTypeNodes = diagramData.linkDataArray?.filter((link: any) => {
-      return link.from === importNode.key;
-    });
-    return docletTypeNodes;
-  };
-
   const infoDrawerBody = () => {
     const diagram: go.Diagram | null | undefined =
       diagramRef?.current?.getDiagram();
 
-    const selNode = diagram?.selection.first();
+    const selImportNode = diagram?.selection.first();
 
-    const docletTypes = diagramData.nodeDataArray?.filter((nodeTypes: any) => {
+    const docletTypes = nodeDataArray?.filter((nodeTypes: any) => {
       return nodeTypes.category === goJsCategory.DocletTypeNode;
     });
 
     if (
-      selNode &&
+      selImportNode &&
       docletTypes &&
-      selNode.category === goJsCategory.ImportNode
+      selImportNode.category === goJsCategory.ImportNode
     ) {
-      const importNodeLinks = getImportNodeLinks(selNode);
+      const importNodeLinks = linkDataArray?.filter(
+        (link: any) => link.from === selImportNode.key,
+      );
+
+      const importNodes = nodeDataArray?.filter((node: any) => {
+        return node.category === goJsCategory.ImportNode;
+      });
 
       return (
         <FormControl component="fieldset">
           <FormLabel component="legend">
             <span>Create or Remove Link from Import Node&nbsp;</span>
-            <strong>{selNode.data.text}</strong>
+            <strong>{selImportNode.data.text}</strong>
             <span>&nbsp;into Doclet Type Node</span>
           </FormLabel>
           <FormGroup aria-label="position">
             {docletTypes.map((docletType: any, idx: number) => {
               let checked = false;
+              let linkedToOther;
 
-              const linked = importNodeLinks.find((link: any) => {
-                return link.to === docletType.key;
-              });
+              const linkedToSelection = importNodeLinks.find(
+                (link: any) => link.to === docletType.key,
+              );
 
-              if (linked) {
+              if (linkedToSelection) {
                 checked = true;
+              } else {
+                linkedToOther = linkDataArray.find((link: any) => {
+                  if (
+                    link.to === docletType.key &&
+                    link.from !== selImportNode.key
+                  ) {
+                    return importNodes?.find((node: any) => {
+                      return node.key === link.from;
+                    });
+                  } else {
+                    return false;
+                  }
+                });
               }
 
               return (
@@ -267,6 +143,7 @@ export default function App() {
                       onChange={handleLinkChange}
                       name={docletType.key}
                       checked={checked}
+                      disabled={linkedToOther ? true : false}
                     />
                   }
                   value={docletType.key}
@@ -285,12 +162,16 @@ export default function App() {
       diagramRef={diagramRef}
       drawerBody={infoDrawerBody()}
       paletteRef={paletteRef}
-      linkDataArray={diagramData.linkDataArray}
-      nodeDataArray={diagramData.nodeDataArray}
+      linkDataArray={linkDataArray}
+      nodeDataArray={nodeDataArray}
       onDiagramEvent={handleDiagramEvent}
-      onModelChange={handleModelChange}
-      paletteData={diagramData.paletteData}
-      skipsDiagramUpdate={diagramData.skipsDiagramUpdate}
+      onModelChange={(obj: any) => {
+        dispatch(onModelChange(obj));
+      }}
+      paletteData={paletteData}
+      skipsDiagramUpdate={skipsDiagramUpdate}
     />
   );
-}
+};
+
+export default App;
